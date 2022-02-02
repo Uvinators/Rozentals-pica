745ae553e66cd3514cbd98f134d183fc86830c1b
Uvis <uvis.rozentals@gmail.com>
2022-02-02 21:57:16 +0200

    Izveidota metozu izvelne

--- src/Pica.java
+++ src/Pica.java
@@ -1,9 +1,47 @@
+import javax.swing.JOptionPane;
 
 public class Pica {
+	
+	public static void pasutitPicu() {
+		
+	}
+	
+	public static void apskatitPicu() {
+		
+	}
+	
+	public static void apskatitCenu() {
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String izvele;
+		
+		do {
+		izvele = JOptionPane.showInputDialog("1 - Pasutit picu | 2 - Apskatit picu | 3 - Apskatit cenu | stop - Apstadinat programmu");
+		izvele=izvele.toLowerCase();
+		
+		switch(izvele) {
+		
+		case "1":
+				pasutitPicu();
+			break;
+		case "2":
+				apskatitPicu();
+			break;
+		case "3":
+				apskatitCenu();
+			break;
+		case "stop":
+			 JOptionPane.showMessageDialog(null, "Programma aptureta", "Bridinajums", JOptionPane.WARNING_MESSAGE);
+			break;
+			default:
+				JOptionPane.showMessageDialog(null, "Darbiba nepastav", "Kluda", JOptionPane.ERROR_MESSAGE);
+		
+		}
+		
+		}while(!izvele.equals("stop"));
 	}
 
 }
